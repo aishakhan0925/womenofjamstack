73849472c1eba44439f49112452503e8c9df105c
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2022-02-23 12:02:14 +0000

    :sparkles: Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.ignores.add("./src/_sass");
+  eleventyConfig.ignores.add("./src/_css/main.css.map");
+
+  eleventyConfig.addWatchTarget("./src/css");
+  eleventyConfig.addPassthroughCopy({ "./src/_css": "/css" });
+  eleventyConfig.addPassthroughCopy({ "./src/_fonts": "/fonts" });
+  eleventyConfig.addPassthroughCopy({ "./src/_public": "/" });
+
+  return {
+    dir: {
+      data: "_data",
+      input: "src",
+      includes: "_includes",
+      layouts: "_layouts",
+    },
+  };
+};
---
+++ src/index.md
@@ -0,0 +1,9 @@
+---
+layout: base.html
+title: Women of Jamstack
+metaDescription:
+  Women of Jamstack celebrates the women who are working with the Jamstack and adjacent technologies — and inspires the
+  next generation of developers in an industry that still suffers from devastating gender inequality.
+---
+
+yolo
